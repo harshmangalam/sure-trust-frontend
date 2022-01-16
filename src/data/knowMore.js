9370ded0a24a7f5d0769859f25bee97350cac226
Harsh Mangalam <hmangalam@oplglobal.com>
2022-01-16 14:04:12 +0530

    know more

---
+++ src/data/knowMore.js
@@ -0,0 +1,38 @@
+export const eligibility = {
+  title: "Eligibility for Enrollment",
+  items: [
+    "B. Tech/ B.E completed or pursuing fourth year, any field",
+    "MBA or PG in any field with flare for learning computer program",
+    "Any undergraduate students who desires to upgrade skills",
+  ],
+};
+
+export const uniqueness = {
+  title: "Uniqueness Of The Course:",
+  items: [
+    "One on One class to ensure that all the students understand the subject.",
+    "Course Report prepared by each student to show case the creative part of the exercises worked, innovative content developed that will exhibit the perseverance of the student and the patience of the trainer.",
+    "Project report covers the procedure followed and the research outcome of the live project handled by each student in each course.",
+    "Commitment, sincerity and hard-work from the trainees, are much appreciated.",
+  ],
+};
+
+export const modalities = {
+  title: "Uniqueness Of the Course",
+  items: [
+    { heading: "Mode of Training", subHeading: "Online" },
+    { heading: "Period of Training", subHeading: "Four months" },
+    { heading: "Sessions per week", subHeading: "3 to 6" },
+    { heading: "Length of the session", subHeading: "1 to 2 hours" },
+    { heading: "Assignments", subHeading: "2 per month" },
+    {
+      heading: "Last 15 days",
+      subHeading: "Preparing the project and course reports",
+    },
+  ],
+};
+
+export const brochure = {
+  title: "SURE  Initiative Brouchure",
+  content: "/pdfs/broucher.pdf",
+};
--- src/screens/main/KnowMore.jsx
+++ src/screens/main/KnowMore.jsx
@@ -1,10 +1,109 @@
-import { Box, Text } from "@chakra-ui/react";
-
+import {
+  Box,
+  Container,
+  Text,
+  Heading,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  List,
+  ListItem,
+  ListIcon,
+  SimpleGrid,
+  GridItem,
+} from "@chakra-ui/react";
+import { MdCheckCircle } from "react-icons/md";
+import {
+  brochure,
+  eligibility,
+  modalities,
+  uniqueness,
+} from "../../data/knowMore";
 function KnowMore() {
   return (
-    <Box>
-      <Text textAlign="center">KnowMore - Coming Soon</Text>
-    </Box>
+    <Container maxW={"container.xl"}>
+      <Heading textAlign="center">APPROACH FOR COURSE COMPLETION</Heading>
+
+      <Box mt={24}>
+        <Heading fontSize={"3xl"} textAlign={"center"}>
+          {modalities.title}
+        </Heading>
+
+        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8} mt={12}>
+          {modalities.items.map((item, i) => (
+            <GridItem key={item}>
+              <Box border="2px" rounded="xl" borderColor="blue.500" p={6}>
+                <Heading fontSize={"xl"} textTransform={"capitalize"}>{item.subHeading}</Heading>
+                <Text>{item.heading}</Text>
+              </Box>
+            </GridItem>
+          ))}
+        </SimpleGrid>
+      </Box>
+
+      <Box mt={24}>
+        <Accordion allowToggle>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  {eligibility.title}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <List spacing={3} size={"xl"}>
+                {eligibility.items.map((item, i) => (
+                  <ListItem key={i}>
+                    <ListIcon as={MdCheckCircle} color="green.500" />
+                    {item}
+                  </ListItem>
+                ))}
+              </List>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+        <Accordion allowToggle>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  {uniqueness.title}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <List spacing={3} size={"xl"}>
+                {uniqueness.items.map((item, i) => (
+                  <ListItem key={i}>
+                    <ListIcon as={MdCheckCircle} color="green.500" />
+                    {item}
+                  </ListItem>
+                ))}
+              </List>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </Box>
+
+      <Box mt={24}>
+        <Heading textAlign={"center"} textTransform={"capitalize"} fontSize={"3xl"}>{brochure.title}</Heading>
+        <Box
+          mt={12}
+          as="object"
+          w="full"
+          h="2xl"
+          data={brochure.content}
+          type="application/pdf"
+        >
+          <Box as="iframe" src={brochure.content} w="full" h="xl"></Box>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
