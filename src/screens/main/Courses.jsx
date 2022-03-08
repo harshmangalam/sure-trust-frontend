0c8694f0f8c1c374560ea72bcdaa2073af98c6b2
Harsh Mangalam <hmangalam@oplglobal.com>
2022-03-08 21:15:28 +0530

    course schedule

--- src/screens/main/Courses.jsx
+++ src/screens/main/Courses.jsx
@@ -1,10 +1,27 @@
-import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import { useQuery } from "react-query";
 import { fetchCourses } from "../../services";
 import CourseCard from "../../components/courses/CourseCard";
 import CoursesSkeleton from "../../components/courses/CoursesSkeleton";
 import Error from "../../components/shared/Error";
+import courseSchedule from "../../assets/course-table.pdf";
+import { useState } from "react";
 function Courses() {
+  const [courseModal, setCourseModal] = useState(false);
   const query = useQuery("courses", fetchCourses);
 
   if (query.isLoading) {
@@ -16,12 +33,53 @@ function Courses() {
   }
   return (
     <Container maxW="container.xl" py={12}>
-      <Heading fontSize={{ base: "4xl", md: "5xl" }} textAlign={"center"}>Courses</Heading>
+      <Flex justify={"space-between"}>
+        <Heading fontSize={"4xl"} textAlign={"center"}>
+          Courses
+        </Heading>
+        <Button
+          rounded={"full"}
+          colorScheme={"twitter"}
+          onClick={() => setCourseModal(true)}
+          display={["none","block"]}
+        >
+          Course Schedule
+        </Button>
+      </Flex>
+
       <SimpleGrid mt={12} columns={{ base: 1, md: 2, lg: 3 }} spacing={6}>
         {query.data.map((course) => (
           <CourseCard course={course} key={course.id} />
         ))}
       </SimpleGrid>
+
+      <Modal
+        onClose={() => setCourseModal(false)}
+        size={"full"}
+        isOpen={courseModal}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Course Schedule</ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody>
+            <Box
+              mt={12}
+              as="object"
+              w="100%"
+              h="70vh"
+              data={courseSchedule}
+              type="application/pdf"
+            >
+              <Box as="iframe" src={courseSchedule} w="full" h="70vh"></Box>
+            </Box>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={() => setCourseModal(false)}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Container>
   );
 }
