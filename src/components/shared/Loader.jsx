47c78762ed069cf30de9904550754d421e023370
Harsh Mangalam <hmangalam@oplglobal.com>
2022-03-10 23:04:48 +0530

    loader changes

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -2,7 +2,7 @@ import { Box, Spinner } from "@chakra-ui/react";
 
 function Loader() {
   return (
-    <Box pos="absolute" top={"50%"} left={"50%"}>
+    <Box pos="relative" minH="100vh">
       <Box pos="absolute" top="50%" left="50%" transform="translate(-50%,-50%)">
         <Spinner label="Loading..." thickness="4px" size="xl" />
       </Box>
