9249781a02f6eb627ab4dc5ce53983cb344351b2
harshmangalam <harshdev8218@gmail.com>
2022-09-14 21:46:22 +0530

    updated theme

--- src/components/shared/ThemeToggle.jsx
+++ src/components/shared/ThemeToggle.jsx
@@ -1,18 +1,20 @@
-import { IconButton, Tooltip, useColorMode } from "@chakra-ui/react";
+import { Box, IconButton, Tooltip, useColorMode } from "@chakra-ui/react";
 import React from "react";
 import { RiMoonFill, RiSunFill } from "react-icons/ri";
 function ThemeToggle() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Tooltip label="Switch Theme">
-      <IconButton onClick={toggleColorMode} aria-label="Toggle Theme">
-        {colorMode === "light" ? (
-          <RiMoonFill size={20} />
-        ) : (
-          <RiSunFill size={20} />
-        )}
-      </IconButton>
-    </Tooltip>
+    <Box>
+      <Tooltip label={colorMode === "dark" ? "Light" : "Dark"}>
+        <IconButton onClick={toggleColorMode} aria-label="Toggle Theme">
+          {colorMode === "light" ? (
+            <RiMoonFill size={20} />
+          ) : (
+            <RiSunFill size={20} />
+          )}
+        </IconButton>
+      </Tooltip>
+    </Box>
   );
 }
 
