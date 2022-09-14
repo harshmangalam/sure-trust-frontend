7600ddfea2e784c65ec76a4330ca92a88e4c260e
harshmangalam <harshdev8218@gmail.com>
2022-09-14 21:31:04 +0530

    added plantation screen

---
+++ src/screens/main/Plantation.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@chakra-ui/react";
+
+export default function Plantation() {
+  return (
+    <Container maxW="container.xl" py={12}>
+      Plantation
+    </Container>
+  );
+}
