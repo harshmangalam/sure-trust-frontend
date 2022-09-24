dd326ef28ad7f48c6c0984639f040bbb1fa97a8c
harshmangalam <harshdev8218@gmail.com>
2022-09-24 20:32:58 +0530

    better scroll msg ui

--- src/components/shared/ScrollingMessages/index.jsx
+++ src/components/shared/ScrollingMessages/index.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Progress } from "@chakra-ui/react";
 import { useQuery } from "react-query";
 import Slider from "react-slick";
 import { fetchNotices } from "../../../services";
@@ -6,10 +6,13 @@ import ScrollText from "./ScrollText";
 export default function ScrollingMessages() {
   const query = useQuery("notices", fetchNotices);
 
+  if (query.isLoading) {
+    return <Progress  isIndeterminate />;
+  }
   return (
-    <Box bg={"blue.500"}  color={"white"} py={2}>
+    <Box bg={"blue.500"} color={"white"} py={2}>
       <Slider autoplay autoplaySpeed={4000} arrows={false}>
-        {query.data?.map((notice, i) => (
+        {query?.data?.map((notice, i) => (
           <ScrollText notice={notice} key={notice.id} />
         ))}
       </Slider>
