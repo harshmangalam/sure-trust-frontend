8e6a679abdbffbb76597647f74b3c2d2e0840b9c
harshmangalam <harshdev8218@gmail.com>
2022-11-18 23:15:08 +0530

    removed ga hooks

--- src/hooks/useGAEventTracker.jsx
+++
@@ -1,9 +0,0 @@
-import ReactGA from "react-ga";
-
-const useGAEventTracker = (category) => {
-  const eventTracker = (action, label) => {
-    ReactGA.event({ category, action, label });
-  };
-  return eventTracker;
-};
-export default useGAEventTracker;
