4337f5ac368a817b2b05e5aa9175945c9aa46fe4
harshmangalam <harshdev8218@gmail.com>
2023-04-02 21:22:49 +0530

    fetch domain details

--- src/services/iery.js
+++ src/services/iery.js
@@ -3,3 +3,8 @@ import axios from "axios";
 export const fetchDomains = async () => {
   return  axios.get("/courses/domain/");
 };
+
+export const fetchDomainDetails = async ({id}) => {
+    return  axios.get(`/courses/get_domain/${id}/`);
+  };
+  
