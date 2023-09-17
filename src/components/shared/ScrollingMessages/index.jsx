b74389112382ca342ff5798fc38c655cfc54a0d7
harshmangalam <harshdev8218@gmail.com>
2023-09-17 13:10:55 +0530

    removed unused scrips and styles loaded during render

--- src/components/shared/ScrollingMessages/index.jsx
+++ src/components/shared/ScrollingMessages/index.jsx
@@ -1,33 +1,39 @@
-import { Box, Progress } from "@chakra-ui/react";
+import { Center, Skeleton, Text } from "@chakra-ui/react";
 import { useQuery } from "react-query";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import { fetchNotices } from "../../../services";
 import ScrollText from "./ScrollText";
+import { useMemo } from "react";
 export default function ScrollingMessages() {
-  const query = useQuery("notices", fetchNotices);
+  const { isLoading, isError, data } = useQuery("notices", fetchNotices);
   const handleDragStart = (e) => e.preventDefault();
+  const notices = useMemo(() => {
+    return data?.map((notice, i) => (
+      <ScrollText
+        onDragStart={handleDragStart}
+        notice={notice}
+        key={notice.id}
+      />
+    ));
+  }, [data]);
 
-  const items = query?.data?.map((notice, i) => (
-    <ScrollText onDragStart={handleDragStart} notice={notice} key={notice.id} />
-  ));
+  if (isError) return <Text>Error while loading notices...</Text>;
+  if (isLoading) return <Skeleton height="40px" />;
 
-  if (query.isLoading) {
-    return <Progress isIndeterminate />;
-  }
   return (
-    <Box bg={"blue.500"} color={"white"} py={2}>
+    <Center bg={"blue.500"} color={"white"} minH={"40px"}>
       <AliceCarousel
         autoPlay
         autoPlayInterval={2000}
         autoPlayStrategy="default"
         mouseTracking
-        items={items}
+        items={notices}
         disableDotsControls
         disableButtonsControls
         controlsStrategy="default"
         infinite
       />
-    </Box>
+    </Center>
   );
 }
