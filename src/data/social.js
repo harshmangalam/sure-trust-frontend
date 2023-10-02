45ac302a05a78a67336d663512c2db5edb5858e9
harshmangalam <harshdev8218@gmail.com>
2023-10-02 09:18:08 +0530

    fix KAN-8

--- src/data/social.js
+++ src/data/social.js
@@ -1,15 +1,9 @@
-import { FaFacebook, FaLinkedinIn } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 
 export const socialLinks = [
-    {
-        name: "Facebook",
-        link: "https://www.facebook.com/sureinitiative/",
-        icon: <FaFacebook size="24px" />
-    },
-
-    {
-        name: "Linkedin",
-        link: "https://www.linkedin.com/in/sure-trust-796a95209?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BvEOGsFxYTpCxdh4%2FKIBMCg%3D%3D",
-        icon: <FaLinkedinIn size="24px" />
-    }
-]
+  {
+    name: "Linkedin",
+    link: "https://www.linkedin.com/company/suretrustforruralyouth/",
+    icon: <FaLinkedinIn size="24px" />,
+  },
+];
