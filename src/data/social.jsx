82c3d03c71b032f1ccf203db1988ca5be35fdcb3
harshmangalam <harshdev8218@gmail.com>
2023-10-09 23:48:23 +0530

    updated linkedin

--- src/data/social.jsx
+++ src/data/social.jsx
@@ -3,7 +3,7 @@ import { FaLinkedinIn } from "react-icons/fa";
 export const socialLinks = [
   {
     name: "Linkedin",
-    link: "https://www.linkedin.com/company/suretrustforruralyouth/",
+    link: "https://www.linkedin.com/company/suretrust-ruralyouth/",
     icon: <FaLinkedinIn size="24px" />,
   },
 ];
