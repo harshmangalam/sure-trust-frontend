aa0efbdf8d56e0267e93424a50f736568b8a0890
harshmangalam <harshdev8218@gmail.com>
2023-12-22 21:29:58 +0530

    updated api

--- src/services/plantation.js
+++ src/services/plantation.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const NODE_BACKEND_URL =
-  import.meta.env.REACT_APP_NODE_ENV === "development"
-    ? import.meta.env.REACT_APP_NODE_DEV_BASEURL
-    : import.meta.env.REACT_APP_NODE_PROD_BASEURL;
+const NODE_BACKEND_URL = "https://suretrust-node.onrender.com";
+// import.meta.env.REACT_APP_NODE_ENV === "development"
+//   ? import.meta.env.REACT_APP_NODE_DEV_BASEURL
+//   : import.meta.env.REACT_APP_NODE_PROD_BASEURL;
 
 export const fetchPlantationCounts = async () => {
   return axios.get(`${NODE_BACKEND_URL}/api/plantation/counts`);
